15928874b9ab7fb48bb19c774a1543d3b426a637
willl <18455527+theonlywilll@users.noreply.github.com>
2019-07-21 19:31:38 -0300

    Removed unless stuff

--- src/data/chapters.py
+++ src/data/chapters.py
@@ -1,30 +1,3 @@
-chaptersbook1 = {
-	1: {
-		'start': 262752,
-		'units': 21,
-		'reinforcements': 'no',
-		'outdoor': 'yes',
-		'enemydata': 1
-	},
-	
-	2: {
-		'start': 295799,
-		'units': 9,
-		'reinforcements': 263152,
-		'unitsr': 13,
-		'outdoor': 'yes',
-		'enemydata': 45
-	},
-	
-	3: {
-		'start': 295971,
-		'units': 8,
-		'reinforcements': 263400,
-		'unitsr': 10,
-		'outdoor': 'yes',
-		'enemydata': 2
-	}
-}
 chapterunits = {
 	1: {
 		'type': 'table',
